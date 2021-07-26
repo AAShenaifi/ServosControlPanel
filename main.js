e38b8c535d3e4fc327574ef5cc9390b79ab234f3
Abdullah AlShenaifi <85587466+AShenaifi@users.noreply.github.com>
2021-07-26 08:00:44 +0300

    JavaScript الملف البرمجي للغة

---
+++ main.js
@@ -0,0 +1,36 @@
+var slider1 = document.getElementsByName("M1");
+var output1 = document.getElementsByName("d1");
+
+function setVal1(v){
+    output1[v].innerHTML = slider1[v].value;
+}
+
+
+var slider2 = document.getElementsByName("M2");
+var output2 = document.getElementsByName("d2");
+
+function setVal2(v){
+    output2[v].innerHTML = slider2[v].value;
+}
+
+
+var slider3 = document.getElementsByName("M3");
+var output3 = document.getElementsByName("d3");
+
+function setVal3(v){
+    output3[v].innerHTML = slider3[v].value;
+}
+
+var slider4 = document.getElementsByName("M4");
+var output4 = document.getElementsByName("d4");
+
+function setVal4(v){
+    output4[v].innerHTML = slider4[v].value;
+}
+
+var slider5 = document.getElementsByName("M5");
+var output5 = document.getElementsByName("d5");
+
+function setVal5(v){
+    output5[v].innerHTML = slider5[v].value;
+}
